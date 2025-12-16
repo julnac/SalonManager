2ce21e2eb334457cb19a4cbbd5ffb053ca82b1cb
julnac <julia.naczke007@gmail.com>
2025-12-16 22:36:18 +0100

    init spring with dependencies

---
+++ salon-manager/salon-manager/src/main/java/pl/edu/salonmanager/salon_manager/SalonManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.salonmanager.salon_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonManagerApplication.class, args);
+	}
+
+}
