3c7291dbf7cafbc4b294507a3aa1015ab8159eae
julnac <julia.naczke007@gmail.com>
2025-12-19 18:55:04 +0100

    service offer request dto

---
+++ salon-manager/salon-manager/src/main/java/pl/edu/salonmanager/salon_manager/model/dto/request/CreateServiceRequest.java
@@ -0,0 +1,28 @@
+package pl.edu.salonmanager.salon_manager.model.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateServiceRequest {
+
+    @NotBlank(message = "Service name is required")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    private BigDecimal price;
+
+    @NotNull(message = "Duration is required")
+    @Positive(message = "Duration must be positive")
+    private Integer durationMinutes;
+}
