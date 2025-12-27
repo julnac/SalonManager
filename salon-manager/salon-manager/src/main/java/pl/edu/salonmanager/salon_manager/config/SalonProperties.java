c844006e8554e630bdf1ce70b3055a7bcea918e1
julnac <julia.naczke007@gmail.com>
2025-12-28 00:26:53 +0100

    add salon opening hours

--- salon-manager/salon-manager/src/main/java/pl/edu/salonmanager/salon_manager/config/SalonProperties.java
+++ salon-manager/salon-manager/src/main/java/pl/edu/salonmanager/salon_manager/config/SalonProperties.java
@@ -1,11 +1,16 @@
 package pl.edu.salonmanager.salon_manager.config;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
+
+import java.time.LocalTime;
 
 @Component
 @ConfigurationProperties(prefix = "salon")
@@ -24,4 +29,15 @@ public class SalonProperties {
 
     @Email
     private String email;
+
+    @NotNull
+    private LocalTime openingTime;
+
+    @NotNull
+    private LocalTime closingTime;
+
+    @NotNull
+    @Min(5)
+    @Max(60)
+    private Integer slotDurationMinutes;
 }
