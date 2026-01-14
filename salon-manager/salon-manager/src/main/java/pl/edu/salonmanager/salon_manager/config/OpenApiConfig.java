870b789b542f576a861960b272095973a761e461
julnac <julia.naczke007@gmail.com>
2026-01-14 13:57:56 +0100

    open api config

--- salon-manager/salon-manager/src/main/java/pl/edu/salonmanager/salon_manager/config/OpenApiConfig.java
+++ salon-manager/salon-manager/src/main/java/pl/edu/salonmanager/salon_manager/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package pl.edu.salonmanager.salon_manager.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +25,6 @@ public class OpenApiConfig {
         Contact contact = new Contact();
         contact.setName("Salon Manager Team");
         contact.setEmail("contact@salonmanager.pl");
-        contact.setUrl("https://github.com/salonmanager");
 
         License license = new License()
                 .name("MIT License")
@@ -31,12 +33,29 @@ public class OpenApiConfig {
         Info info = new Info()
                 .title("Salon Manager API")
                 .version("1.0")
-                .description("REST API dla systemu rezerwacji salonu beauty")
+                .description("REST API dla systemu rezerwacji salonu beauty\n\n" +
+                        "**Uwierzytelnianie:**\n" +
+                        "- Publiczne GET endpoints nie wymagają autoryzacji\n" +
+                        "- Pozostałe endpointy wymagają HTTP Basic Auth\n" +
+                        "- ADMIN role: admin@salon.pl / admin123\n" +
+                        "- USER role: zobacz DataInitializer dla przykładowych użytkowników lub zarejestruj się")
                 .contact(contact)
                 .license(license);
 
+        SecurityScheme basicAuthScheme = new SecurityScheme()
+                .name("basicAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("basic")
+                .description("HTTP Basic Authentication - użyj email i hasła");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("basicAuth");
+
         return new OpenAPI()
                 .info(info)
-                .servers(List.of(localServer));
+                .servers(List.of(localServer))
+                .components(new Components()
+                        .addSecuritySchemes("basicAuth", basicAuthScheme))
+                .addSecurityItem(securityRequirement);
     }
 }
